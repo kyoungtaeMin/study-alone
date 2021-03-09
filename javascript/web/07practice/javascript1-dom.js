b675d3b473986647d2f0d7271b165a0924357773
kyoungtaeMin <minkyoungtae14@gmail.com>
2021-03-09 16:15:49 +0900

    2021-03-09 javascript adding, removing, cloning of Node, Element study

--- javascript/web/07practice/javascript1-dom.js
+++ javascript/web/07practice/javascript1-dom.js
@@ -1,14 +1,138 @@
-// EX5 : Changing Attributes of Element Nodes
+//EX8 : Node cloning and Template tagging
+window.addEventListener("load", function() {
+  var notices = [
+    {id : 5, title : "빅데이터 재밌니?", regDate : "2021-03-05", writerId : "min", hit : 54},
+    {id : 6, title : "개발자는 어떠냐?", regDate : "2021-03-04", writerId : "min", hit : 22}
+  ];
+  var section = document.querySelector("#section8");
+
+  var noticeList = section8.querySelector(".notice-list");
+  var tbodyNode = noticeList.querySelector("tbody");
+  var cloneBtn = section8.querySelector(".clone-btn");
+  var templateBtn = section8.querySelector(".template-btn");
+
+  cloneBtn.onclick = function() {
+    var trNode = noticeList.querySelector("tbody tr");
+    var cloneNode = trNode.cloneNode(true);
+
+    var tds = cloneNode.querySelectorAll("td");
+    tds[0].textContent = notices[0].id;
+    tds[1].innerHTML = '<a href="'+ notices[0].id +'">'+ notices[0].title+"</a>";
+    tds[2].textContent = notices[0].regDate;
+    tds[3].textContent = notices[0].writerId;
+    tds[4].textContent = notices[0].hit;
+
+    trNode.append(cloneNode);
+  }
+
+  templateBtn.onclick = function() {
+    var template = section8.querySelector("template");
+    var cloneNode = document.importNode(template.content, true);
+
+    var tds = cloneNode.querySelectorAll("td");
+    tds[0].textContent = notices[0].id;
+
+    tds[1].innerHTML = '<a href="'+ notices[0].id +'">'+ notices[0].title+"</a>";
+    var aNode = tds[1].children[0];
+    aNode.href = notices[0].id;
+    aNode.textContent = notices[0].title;
+
+    tds[2].textContent = notices[0].regDate;
+    tds[3].textContent = notices[0].writerId;
+    tds[4].textContent = notices[0].hit;
+
+    tbodyNode.append(cloneNode);
+  }
+});
+
+// EX7 : Append Element Nodes
+window.addEventListener("load", function() {
+  var section7 = document.querySelector("#section7");
+
+  var titleInput = section7.querySelector(".title-input");
+  var menuListUl = section7.querySelector(".menu-list");
+  var addBtn = section7.querySelector(".add-btn");
+  var delBtn = section7.querySelector(".del-btn");
+
+  addBtn.onclick = function() {
+    var title = titleInput.value;
+
+    var html = '<a href="">' + title + "</a>";
+    var li = document.createElement("li");
+    li.innerHTML = html;
+
+    //menuListUl.appendChild(li);
+    menuListUl.append(li);
+
+    //menuListUl.innerHTML += '<li><a href="">' + title + "</a></li>";
+    // 위의 코드를 +를 사용하여 누적형으로 만들면 코드 짜기는 편하지만 계속해서 더 큰 문자열을
+    // node로 만들기 때문에 성능상 문제가 생길 수 있다. 귀찮지만 아래 처럼 해야 할 때도 있다
+
+    // var title = titleInput.value;
+    // var textNode = document.createTextNode(title);
+
+    // var aNode = document.createElement("a");
+    // aNode.href="";
+    // aNode.appendChild(textNode);
+
+    // var liNode = document.createElement("li");
+    // liNode.appendChild(aNode);
+
+    // menuListUl.appendChild(liNode);
+
+    
+    // var title = titleInput.value;
+    // var textNode = document.createTextNode(title);
+    // menuListDiv.appendChild(textNode);
+  }
+
+  delBtn.onclick = function() {
+    //var textNode = menuListUl.childNodes[0]; // 얘는 모든 Node들을 대상으로
+    var liNode = menuListUl.children[0]; // 얘는 Element Node만을 선별해서 골라준다
+    //menuListUl.removeChild(liNode);
+    liNode.remove();
+  }
+
+});
+
+// EX6 : Nodes Manipulation : Add Menu(createNode, Element)
+window.addEventListener("load", function() {
+  var section6 = document.querySelector("#section6");
+
+  var titleInput = section6.querySelector(".title-input");
+  var menuListDiv = section6.querySelector(".menu-list");
+  var addBtn = section6.querySelector(".add-btn");
+  var delBtn = section6.querySelector(".del-btn");
+
+  addBtn.onclick = function() {
+    var title = titleInput.value;
+    var textNode = document.createTextNode(title);
+    menuListDiv.appendChild(textNode);
+  }
+
+  delBtn.onclick = function() {
+    var textNode = menuListDiv.childNodes[0];
+    menuListDiv.removeChild(textNode);
+  }
+
+});
+
+// EX5 : Changing Attributes of Element Nodes And Attributes of Css
 window.addEventListener("load", function() {
   var section5 = document.querySelector("#section5");
   var srcInput = section5.querySelector(".src-input");
   var imgSelect = section5.querySelector(".img-select");
   var changeBtn = section5.querySelector(".change-btn");
   var img = section5.querySelector(".img");
+  var colorInput = section5.querySelector(".color-input");
 
   changeBtn.onclick = function() {
     img.src = "../images/"+srcInput.value;
     //img.src = "../images/"+imgSelect.value;
+
+    //img.style["border-color"] = colorInput.value; // key 값으로 만들어 이용할 수 있다
+    img.style.borderColor = colorInput.value; // dash는 구분자로 사용하지 못하기 때문에 dash다음에 오는 문자를 대문자로 만들어서 사용할 수 있다.
+    console.log(img.className);
   }
 
 });
