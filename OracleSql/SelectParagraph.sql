60c04c2b7836723d10fd45ff6c15444db9664828
kyoungtaeMin <minkyoungtae14@gmail.com>
2021-03-11 17:49:33 +0900

    2021-03-11 having, row_number(), rank() etc.. study

--- OracleSql/SelectParagraph.sql
+++ OracleSql/SelectParagraph.sql
@@ -19,6 +19,7 @@ SELECT WRITER_ID, COUNT(TITLE) COUNT FROM NOTICE GROUP BY WRITER_ID;
 
 SELECT WRITER_ID, COUNT(TITLE) COUNT 
 FROM NOTICE 
+--WHERE
 GROUP BY WRITER_ID
 ORDER BY COUNT DESC;
 
@@ -39,6 +40,47 @@ GROUP BY WRITER_ID; --에러
 SELECT WRITER_ID, COUNT(N.ID) FROM NOTICE N
 GROUP BY WRITER_ID
 HAVING COUNT(N.ID) < 2; -- 정상적으로 분석값 반환
+
+---------- ROW_NUMBER(), RANK(), PARTITION ---------------------------------
+SELECT ROW_NUMBER() OVER (ORDER BY HIT), ID, TITLE, WRITER_ID, REGDATE, HIT
+FROM NOTICE;
+--WHERE 절 쯤에서 만들어 지기 때문에 ORDER BY에 적용되지 않는다.
+--GROUP BY
+--HAVING
+--ORDER BY HIT;
+-- ROWNUM을 함수화 시켜놓은 ROW_NUMBER() OVER를 사용하여 정렬 시킬 수 있다.
+
+SELECT RANK() OVER (ORDER BY HIT), ID, TITLE, WRITER_ID, REGDATE, HIT
+FROM NOTICE;
+-- 정렬된 상태로 등수를 매기는 함수
+SELECT DENSE_RANK() OVER (ORDER BY HIT), ID, TITLE, WRITER_ID, REGDATE, HIT
+FROM NOTICE;
+-- 공동 순위에 다음에 오는 등수를 건너 뛰지 않고 바로 다음 등수로 매기는 함수
+
+SELECT ROW_NUMBER() OVER (PARTITION BY WRITER_ID ORDER BY HIT DESC),
+ID, TITLE, WRITER_ID, REGDATE, HIT
+FROM NOTICE;
+-- 지정한 컬럼을 기준으로 그룹을 만들어 그 안에서 등수를 매기는 함수
+
+---------- SUB QEURY -------------------------------------------
+SELECT * FROM NOTICE WHERE ROWNUM BETWEEN 1 AND 10;
+
+SELECT * FROM NOTICE 
+ORDER BY REGDATE DESC
+WHERE ROWNUM BETWEEN 1 AND 10; -- ERORR 그럼 어떻게해?
+
+SELECT * FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC) 
+WHERE ROWNUM BETWEEN 1 AND 10;
+-- 원하는 결과를 끌어내기 위해 식의 순서를 바꿔야 할 때 사용 할 수 있다.
+-- 평생 쓸꺼같으니까 연습 많이 하자....
+
+SELECT * FROM MEMBER WHERE AGE >= 30;
+
+SELECT * FROM MEMBER 
+WHERE AGE >= (SELECT AVG(AGE) FROM MEMBER);
+-- 먼저 평균나이를 구해야 하니까 구하고 나서 다음 조건 진행
+
+---------- JOIN -------------------------------------------
 
 SELECT * FROM MEMBER;
 
