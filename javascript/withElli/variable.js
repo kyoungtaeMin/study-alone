d4fd287ca9e07496de4b0968b2de0a7c70461e27
kyoungtaeMin <minkyoungtae14@gmail.com>
2021-03-13 02:48:55 +0900

    2021-03-13 variable, operator, object, loops

---
+++ javascript/withElli/variable.js
@@ -0,0 +1,119 @@
+// 1. Use strict
+// added in ES5
+// use this for Valina Javascript.
+"use strict"
+
+// 2. Variable, rw(read/write)
+// let (added in ES6)
+let globalName = 'global name';
+{
+  let name = 'boy';
+  console.log(name);
+  name = 'girl';
+  console.log(name);
+  console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+// var (don't ever use this!)
+// hoisting (move, declaration from bottom to top)
+// 어디에 선언했는가에 상관없이 항상 제일 위로 선언을 끌어올려주는 것
+// has no block scope
+console.log(age);
+{
+  age = 4;
+  var age;
+}
+console.log(age);
+
+// 3. Constant, r(read only)
+// use const whenever possible
+// only use let if variable needs to change
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// Note!!!
+// Immutable data types: premitive types, frozen objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
+// favor immutable data type always for a few reasons:
+// - security
+// - thread safety
+// - reduce human mistake
+
+// 4. Variable types
+// primitive, single item : number, string, boolean, null, undefined, symbol
+// object, box container
+// function, first-class function
+const count = 17; // integer
+const size = 17.1; // decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// number - speicla numeric values : infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet)
+const bigInt = 123456789012345678901234567890n; // over (-2**53) ~ (2**53)
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+
+// string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello ' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; // template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true : any other value
+const canRead = true;
+const test = 3 < 1;
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x = undefined;
+let y;
+console.log(`value: ${x}, type: ${typeof x}`);
+console.log(`value: ${y}, type: ${typeof y}`);
+
+// symbol, create unique idintifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); //true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+
+// object, real-life object, data structure
+const ellie = {name : 'ellie', age : 20};
+ellie.age = 21; // ellie 는 const로 선언되어 변경 불가하지만 그 안의 속성인 name, age는 변경가능
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); // h
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+// 문자와 숫자를 더하면 숫자를 문자로 변환해서 연산 후 반환
+text = '8' / '2'
+console.log(`value: ${text}, type: ${typeof text}`);
+// 문자끼리 나누면 숫자로 변환하여 연산 후 반환
+text = '8' / 2
+console.log(`value: ${text}, type: ${typeof text}`);
+// 문자와 숫자를 나누면 숫자로 변환하여 연산 후 반환
+// console.log(text.charAt(0)); // error
